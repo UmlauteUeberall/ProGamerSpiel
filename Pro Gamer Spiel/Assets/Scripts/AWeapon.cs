0bd2ffc61d47b039924cb3ad1449ac55f39f28b8
DragonMagic <DragonMagic>
2016-06-28 13:12:04 +0200

    Cannon Controller Script and Cannon

--- Pro Gamer Spiel/Assets/Scripts/AWeapon.cs
+++ Pro Gamer Spiel/Assets/Scripts/AWeapon.cs
@@ -7,45 +7,13 @@ public abstract class AWeapon : MonoBehaviour
 
     public int m_Damage;
     public int m_CurrentAmmunition;
-    public int m_MagazineCap;
-    public int m_MaxAmmo;
-    
 
 
     protected virtual void Awake()
     {
         m_owner = GetComponent<AEntity>();
+
     }
 
     public abstract void Shoot(Vector3 _start, Vector3 _dir);
 }
-
-
-/*
-Der Bereich wo der Schuss hinfallen kann wird zufällig gesetzt
-            float randomRadius = maxSpreadRadius;
-randomRadius = Random.Range(0, maxSpreadRadius);
-
-            // Der Winkel und die tatsächliche Richtung des Schusses wird gesetzt
-            float randomAngle = Random.Range(0, 2 * Mathf.PI);
-
-Vector3 direction = new Vector3(
-    randomRadius * Mathf.Cos(randomAngle),
-    randomRadius * Mathf.Sin(randomAngle),
-    15);
-
-direction = Camera.main.transform.TransformDirection(direction.normalized);
-
-            // Der eigentliche Schuss findet hier statt
-            RaycastHit hit;
-Ray ray = new Ray(Camera.main.transform.position, direction);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                gc.bulletPoint.transform.position = hit.point;
-                if (hit.transform.tag == "Enemy")
-                {
-                    hit.transform.GetComponent<EnemyController>().TakeDamage();
-                }
-            }
-            */
---
+++ Pro Gamer Spiel/Assets/Scripts/CannonControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonControl : MonoBehaviour 
+{
+    public int m_PivotxMin;
+    public int m_PivotxMax;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Rotation();
+	}
+    void Rotation()
+    {
+        transform.Rotate(new Vector3(1, 5, 3) * (Time.deltaTime));
+    }
+}
