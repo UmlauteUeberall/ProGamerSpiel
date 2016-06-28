8e2fcbe2d36215b3ce6bf54d492f280d2ecc0d21
Markus Baars <Markus Baars>
2016-06-28 14:02:05 +0200

    AWeapon geändert!

--- Pro Gamer Spiel/Assets/Scripts/AWeapon.cs
+++ Pro Gamer Spiel/Assets/Scripts/AWeapon.cs
@@ -18,9 +18,9 @@ public abstract class AWeapon : MonoBehaviour
         m_owner = GetComponent<AEntity>();
     }
 
-    public virtual void Shoot(bool shoot)
+    public virtual void Shoot(Vector3 _start, Vector3 _dir)
     {
-        if (shoot && m_WeaponCoolDown <= 0)
+        if (m_WeaponCoolDown <= 0)
         {
             float randomRadius = m_MaxSpreadRadius;
             randomRadius = Random.Range(0, m_MaxSpreadRadius);
