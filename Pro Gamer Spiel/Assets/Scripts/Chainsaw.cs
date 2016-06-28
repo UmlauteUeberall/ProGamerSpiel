a760b9ea2c30408cf87d94d2572bfb220f0c710e
unknown <kingeldarion@live.de>
2016-06-28 13:40:31 +0200

    Lasergund und Chainsaw hinzugefügt

---
+++ Pro Gamer Spiel/Assets/Scripts/Chainsaw.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chainsaw : AWeapon {
+
+    private float maxDistance = 5;
+
+    private float ammunitionSeconds = 0;
+
+    void Awake(){
+
+        base.Awake();
+
+        m_Damage = 35;
+        m_CurrentAmmunition = 15;
+    }
+
+    public override void Shoot(Vector3 _start, Vector3 _dir){
+
+        Ray ray = new Ray(_start, _dir);
+        RaycastHit hit;
+
+        ammunitionSeconds -= Time.deltaTime;
+
+        if (ammunitionSeconds <= 0) {
+
+            ammunitionSeconds += 1;
+            m_CurrentAmmunition--;
+        }
+
+        if (m_CurrentAmmunition >= 0 && Physics.Raycast(ray, out hit)) {
+
+            if (Vector3.Distance(transform.position, hit.point) < maxDistance) {
+
+                hit.transform.gameObject.GetComponent<AEntity>().TakeDamage(m_Damage * Time.deltaTime);
+            }
+        }
+    }
+}
--- Pro Gamer Spiel/Assets/Scripts/LaserGun.cs
+++ Pro Gamer Spiel/Assets/Scripts/LaserGun.cs
@@ -3,7 +3,34 @@ using System.Collections;
 
 public class LaserGun : AWeapon {
 	
-    private float damage;
+    private float curAmmunitionSeconds = 0;
+
+    private LineRenderer lineRenderer;
+
+    private float disableLineRenderer = -1;
+
+    void Awake(){
+
+        base.Awake();
+
+        m_CurrentAmmunition = 15;
+        m_Damage = 10;
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update(){
+
+        if (disableLineRenderer <= 0) {
+
+            lineRenderer.enabled = false;
+        }
+        else {
+
+            disableLineRenderer -= Time.deltaTime;
+        }
+
+        Shoot(transform.position, Vector3.forward);
+    }
 
     public override void Shoot(Vector3 _start, Vector3 _dir){
 
@@ -11,14 +38,29 @@ public class LaserGun : AWeapon {
 
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit)) {
+        lineRenderer.enabled = true;
+
+        if (m_CurrentAmmunition >= 0 && Physics.Raycast(ray, out hit)) {
 
             AEntity entity = transform.gameObject.GetComponent<AEntity>();
+            lineRenderer.SetPositions(new Vector3[]{ transform.position, hit.point });
 
             if (entity != null) {
                 
-                entity.TakeDamage(damage * Time.deltaTime);
+                entity.TakeDamage(m_Damage * Time.deltaTime);
             }
+        }
+        else {
+
+            lineRenderer.SetPositions(new Vector3[]{ transform.position, _start + _dir.normalized * 25 });
+        }
+
+        curAmmunitionSeconds -= Time.deltaTime;
+
+        if (curAmmunitionSeconds <= 0) {
+
+            m_CurrentAmmunition--;
+            curAmmunitionSeconds += 1;
         }
     }
 }
