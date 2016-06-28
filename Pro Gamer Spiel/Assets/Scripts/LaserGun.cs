8c20d4e1a5de650cc1f887e20aa7f45fb8499974
unknown <kingeldarion@live.de>
2016-06-28 12:55:53 +0200

    LaserGun

---
+++ Pro Gamer Spiel/Assets/Scripts/LaserGun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGun : AWeapon {
+	
+    private float damage;
+
+    public void Shoot(Vector3 _start, Vector3 _dir){
+
+        Ray ray = new Ray(_start, _dir);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+
+            transform.gameObject.GetComponent<AEntity>();
+        }
+    }
+}
