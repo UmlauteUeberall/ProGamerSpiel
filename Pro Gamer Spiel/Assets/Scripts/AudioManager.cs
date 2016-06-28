80a65cffc0848fb72f5bc36e7efdcfe1720c8381
Marc <marcfriedrich1@gmx.de>
2016-06-28 14:17:29 +0200

    laser sound

--- Pro Gamer Spiel/Assets/Scripts/AudioManager.cs
+++ Pro Gamer Spiel/Assets/Scripts/AudioManager.cs
@@ -5,6 +5,8 @@ public class AudioManager
     private static GameObject m_ambient;
     private static GameObject m_ambientSceneObject;
     private static GameObject m_gunShot;
+    private static GameObject m_laserShot;
+    private static GameObject m_laserShotSceneObject;
     private static Camera m_mainCamera;
 
     //set the audio files and the main camera
@@ -13,6 +15,7 @@ public class AudioManager
     {
         m_ambient = Resources.Load<GameObject>("ambient");
         m_gunShot = Resources.Load<GameObject>("gunShot");
+        m_laserShot = Resources.Load<GameObject>("laserShot");
         m_mainCamera = Camera.main;
 	}
 
@@ -59,5 +62,40 @@ public class AudioManager
     {
         GameObject.Instantiate(m_gunShot, _position,
             Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Play the laser shot sound
+    /// at the Audio Listener position
+    /// </summary>
+    public static void PlayLaserShot()
+    {
+        m_laserShotSceneObject = 
+            (GameObject)GameObject.Instantiate(m_laserShot,
+            m_mainCamera.transform.position,
+            m_mainCamera.transform.rotation);
+    }
+
+    /// <summary>
+    /// Play the laser shot sound
+    /// at the Vector3 position
+    /// </summary>
+    /// <param name="_position">Vector3 position</param>
+    public static void PlaylaserShot(Vector3 _position)
+    {
+        m_laserShotSceneObject = 
+            (GameObject)GameObject.Instantiate(m_laserShot,
+            _position, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// stops playing the laser shot sound
+    /// </summary>
+    public static void StopLaserShot()
+    {
+        if (m_laserShotSceneObject != null)
+        {
+            GameObject.Destroy(m_laserShotSceneObject);
+        }
     }
 }
